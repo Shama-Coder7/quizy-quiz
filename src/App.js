244f155329e3f7edcbd577477543a79f681f3f11
Shama Parveen <85455446+Shama-Coder7@users.noreply.github.com>
2021-08-08 20:31:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Quiz from "./Quiz";
+import Result from "./Result";
+
+function App() {
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTV8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80")' }}>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
+          </Route>
+          <Route path="/quiz">
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
+          </Route>
+          <Route path="/result">
+            <Result name={name} score={score} />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
